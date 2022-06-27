9b09f28479cf3b1c1ae8119d104dbf47c1ee092c
cyanidium1 <99473719+cyanidium1@users.noreply.github.com>
2022-06-27 20:30:25 +0300

    version 1.1 some fixes

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+export default 
+    class Counter extends Component {
+        static defaultProps = {
+          step: 1,
+        };
+      
+        render() {
+          const { step } = this.props;
+      
+          return (
+            <div>
+              <span>0</span>
+              <button type="button">Increment by {step}</button>
+              <button type="button">Decrement by {step}</button>
+            </div>
+          );
+        }
+      }
